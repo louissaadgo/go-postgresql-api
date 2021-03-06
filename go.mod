6e2d747f2467a4ea320aacf3dfd4e55d6d635b41
louissaadgo <louissaadgo@gmail.com>
2021-03-06 16:31:32 +0200

    Added all routes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/louissaadgo/go-postgresql-api
 
 go 1.16
 
-require github.com/lib/pq v1.9.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.9.0
+)
--- main.go
+++ main.go
@@ -3,15 +3,48 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
-func main() {
+var db *sql.DB
+
+const port string = ":3024"
+
+func init() {
 	db, err := sql.Open("postgres", "postgres://postgres:2400@localhost/library?sslmode=disable")
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
+	fmt.Println("Connected to the database")
 	defer db.Close()
+}
+
+func main() {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/books", books).Methods("GET")
+	mux.HandleFunc("/authors", authors).Methods("GET")
+	mux.HandleFunc("/new/author", newAuthor).Methods("POST")
+	mux.HandleFunc("/new/book", newBook).Methods("POST")
+	log.Fatal(http.ListenAndServe(port, mux))
+}
+
+func books(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func authors(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func newAuthor(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func newBook(w http.ResponseWriter, r *http.Request) {
+
 }
