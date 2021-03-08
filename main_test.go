ee0742c735bcadfd77cb43a07f9714fcbfa9e2c9
louissaadgo <louissaadgo@gmail.com>
2021-03-08 15:31:08 +0200

    Added test cases for the new routes

--- main_test.go
+++ main_test.go
@@ -15,7 +15,53 @@ func router() *mux.Router {
 	mux := mux.NewRouter()
 	mux.HandleFunc("/new/author", NewAuthor).Methods("POST")
 	mux.HandleFunc("/new/book", NewBook).Methods("POST")
+	mux.HandleFunc("/author/{id}", AuthorByID).Methods("GET")
+	mux.HandleFunc("/book/{id}", BookByID).Methods("GET")
 	return mux
+}
+
+func TestBookByID(t *testing.T) {
+	DB, err = sql.Open("postgres", "postgres://postgres:2400@localhost/library?sslmode=disable")
+	assert.Equal(t, nil, err, "Failed to connect to the database")
+	defer DB.Close()
+	req, _ := http.NewRequest("GET", "/book/1", nil)
+	res := httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+	assert.Equal(t, 200, res.Code, "Failed to handle CORRECT data")
+	req, _ = http.NewRequest("GET", "/book/0", nil)
+	res = httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+	assert.Equal(t, 400, res.Code, "Failed to handle INCORRECT data")
+	req, _ = http.NewRequest("GET", "/book/test", nil)
+	res = httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+	assert.Equal(t, 400, res.Code, "Failed to handle INCORRECT data")
+	req, _ = http.NewRequest("GET", "/book/99999999", nil)
+	res = httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+	assert.Equal(t, 400, res.Code, "Failed to handle INCORRECT data")
+}
+
+func TestAuthorByID(t *testing.T) {
+	DB, err = sql.Open("postgres", "postgres://postgres:2400@localhost/library?sslmode=disable")
+	assert.Equal(t, nil, err, "Failed to connect to the database")
+	defer DB.Close()
+	req, _ := http.NewRequest("GET", "/author/1", nil)
+	res := httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+	assert.Equal(t, 200, res.Code, "Failed to handle CORRECT data")
+	req, _ = http.NewRequest("GET", "/author/0", nil)
+	res = httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+	assert.Equal(t, 400, res.Code, "Failed to handle INCORRECT data")
+	req, _ = http.NewRequest("GET", "/author/test", nil)
+	res = httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+	assert.Equal(t, 400, res.Code, "Failed to handle INCORRECT data")
+	req, _ = http.NewRequest("GET", "/author/99999999", nil)
+	res = httptest.NewRecorder()
+	router().ServeHTTP(res, req)
+	assert.Equal(t, 400, res.Code, "Failed to handle INCORRECT data")
 }
 
 func TestNewAuthor(t *testing.T) {
